a8aef4d1d1f671236ba31da1e25c2dd355a8022e
suryapal123 <155968757+suryapal123@users.noreply.github.com>
2026-03-28 20:42:55 +0530

    Update urls.py

--- mca_erp/mca_erp/urls.py
+++ mca_erp/mca_erp/urls.py
@@ -20,7 +20,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 path('admin/', admin.site.urls),
-path('',include('analytics.urls')),
+path('',include('mca_erp.analytics.urls')),
 
 path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
